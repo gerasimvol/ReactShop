5186c35c4afb6f0ffae41d8864ad5d5db75b26dc
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2018-03-14 21:40:32 +0200

    Add first component

---
+++ src/components/StorePicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class StorePicker extends React.Component {
+  render () {
+    return <h1>{ this.props.name }</h1>
+  }
+}
+
+export default StorePicker
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker'
+
+
+const app = document.querySelector('#main')
+render(<StorePicker name="hello" />, app)
