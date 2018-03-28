cb746787d0065566d99c3968afb5ff96d259fe8b
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2018-03-28 20:39:12 +0300

    Add helper store name

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
   render () {
     return (
       <form className="store-selector">
         <h1>Please Enter A Store</h1>
-        <input type="text" required placeholder="Enter store name" />
+        <input
+          type="text"
+          required
+          placeholder="Enter store name"
+          defaultValue={ getFunName() }
+        />
         <button type="submit">Visit Store ➡</button>
       </form>
     )
