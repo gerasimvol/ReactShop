a197542a1b0af786e3694e29428eb5fff2e3ab29
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2018-04-03 20:35:22 +0300

    Add redirect

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  storeName = React.createRef()
+
+  goToStore = event => {
+    event.preventDefault()
+    this.props.history.push(`/store/${this.storeName.value.value}`)
+  }
+
   render () {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ this.goToStore }>
         <h1>Please Enter A Store</h1>
         <input
           type="text"
           required
+          ref={this.storeName}
           placeholder="Enter store name"
           defaultValue={ getFunName() }
         />
